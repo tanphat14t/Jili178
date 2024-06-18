feb521e3a735864ebf63f6c49cb055f876393eeb
Lucius <tanphat14t@gmail.com>
2024-06-18 16:02:17 +0700

    remove plugin

--- src/wp-content/plugins/formidable-views/classes/views/editor/invalid.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'You are not allowed to call this page directly.' );
-}
-?>
-<script type="text/javascript">
-	window.location = 'edit.php?post_type=frm_display';
-</script>
